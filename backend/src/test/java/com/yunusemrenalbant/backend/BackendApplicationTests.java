bcd88275ec13513b62b28462f057ee0287bf4781
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2024-11-08 15:11:54 +0300

    Initial commit with backend and frontend projects

---
+++ backend/src/test/java/com/yunusemrenalbant/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunusemrenalbant.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
