a1640ba31e60167d0750e19217f5982722f6ac95
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2024-11-10 00:00:53 +0300

    feat: add timestamp to ChatMessage and update ChatController to set message time

--- backend/src/main/java/com/yunusemrenalbant/backend/controller/ChatController.java
+++ backend/src/main/java/com/yunusemrenalbant/backend/controller/ChatController.java
@@ -7,12 +7,15 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
+
 @Controller
 public class ChatController {
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        chatMessage.setTime(LocalDateTime.now());
         return chatMessage;
     }
 
--- backend/src/main/java/com/yunusemrenalbant/backend/model/ChatMessage.java
+++ backend/src/main/java/com/yunusemrenalbant/backend/model/ChatMessage.java
@@ -4,6 +4,8 @@ package com.yunusemrenalbant.backend.model;
 import com.yunusemrenalbant.backend.enums.MessageType;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,6 +15,7 @@ public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
+    private LocalDateTime time;
 }
 
 
